df6238f5c542a856e110e4ee5adbbd2d79ca5e87
maxaaa9 <maxaaa9@abv.bg>
2024-06-13 14:45:02 +0300

    Exercise

---
+++ Subqueries_and_JOINs_Exercise/08_Hahn`s_Bookings.sql
@@ -0,0 +1,6 @@
+SELECT
+	COUNT(*)
+FROM
+	customers AS c
+WHERE
+	c.last_name LIKE 'Hahn'
---
+++ Subqueries_and_JOINs_Exercise/11_Bulgaria's_Peaks_Higher_than_2835_Meters.sql
@@ -0,0 +1,21 @@
+SELECT
+	mc.country_code,
+	m.mountain_range,
+	p.peak_name,
+	p.elevation
+FROM
+	mountains AS m
+	JOIN
+		peaks AS p
+		ON m.id = p.mountain_id
+			JOIN
+			mountains_countries AS mc
+			ON mc.mountain_id = m.id
+
+WHERE
+	p.elevation > 2835
+	AND
+	mc.country_code = 'BG'
+
+ORDER BY
+	p.elevation DESC;
